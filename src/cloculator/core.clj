71438ed03c0c74f9ee3f3ffc7222868a981f955c
Lewis Reid <lreidwilliams@gmail.com>
2019-09-15 12:55:50 +1000

    basic adding!

--- src/cloculator/core.clj
+++ src/cloculator/core.clj
@@ -2,8 +2,12 @@
   (:gen-class))
 
 (defn -main []
-  (defn cloculate [expression] (clojure.string/split "Hello Hello" #" "))
-  (doseq [item (cloculate "1 + 1")] (println item)))
+  (defn parse-int [s] (Integer. (re-find  #"\d+" s)))
+  (defn evaluate_operator [expression, operator]
+    ((resolve (symbol operator)) (parse-int expression) (parse-int (subs expression 2))))
+  (defn cloculate [expression] 
+    (evaluate_operator expression "+"))
+  (println (cloculate "1+1")))
 
   
   
